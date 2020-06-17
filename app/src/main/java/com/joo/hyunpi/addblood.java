2d6aa9d56463ad26541c173e3b737e42f7662101
joodevelop <joodevelop@naver.com>
2020-06-17 19:37:46 +0900

    200515

---
+++ app/src/main/java/com/joo/hyunpi/addblood.java
@@ -0,0 +1,26 @@
+package com.joo.hyunpi;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.web3j.protocol.Web3j;
+
+public class addblood extends AppCompatActivity {
+
+
+
+
+
+
+
+    public static void setOnClickListener(View.OnClickListener onClickListener) {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addblood);
+    }
+}
