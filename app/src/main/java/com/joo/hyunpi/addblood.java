971d00dd5ee51249d183bd71a889ae463213dda7
joo <joodevelop@naver.com>
2020-06-21 19:01:22 +0900

    200515

--- app/src/main/java/com/joo/hyunpi/addblood.java
+++ app/src/main/java/com/joo/hyunpi/addblood.java
@@ -2,10 +2,15 @@ package com.joo.hyunpi;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class addblood extends AppCompatActivity {
+
+    public void ButtonClick2(View v) {
+        Toast.makeText(getApplicationContext(), "헌혈증 등록이 완료되었습니다.", Toast.LENGTH_LONG).show();
+    }
 
 
 
